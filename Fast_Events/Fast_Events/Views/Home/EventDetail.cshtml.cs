942f1f58e968192bdc2269af14fd6cade8bf50ed
adrien-guell <adrien.guell@hotmail.fr>
2021-04-26 20:58:48 +0200

    fix crash on launch

--- Fast_Events/Fast_Events/Views/Home/EventDetail.cshtml.cs
+++ Fast_Events/Fast_Events/Views/Home/EventDetail.cshtml.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Fast_Events.Views.Home
 {
     public class EventDetail : PageModel
     {
+        public String imagePath { get; set; }
+        public Boolean isOwner { get; set; }
+        public String location { get; set; }
+        public String googleMapLink { get; set; }
+
+        EventDetail()
+        {
+        }
+        
         public void OnGet()
         {
-            
+            imagePath = @"..\Resources\Images\" + "event_place_holder.jpg";
+            isOwner = true;
+            location = "1 Rue Voltaire, 94270, Le Kremlin Bicetre".Replace(" ", "+");
+            googleMapLink = "https://www.google.com/maps/search/?api=1&query=" + location;
         }
     }
 }
