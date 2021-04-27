cc5f1eecf7b9a4024e2e887d872e81c97038e8d9
louis.arrignon <louis.arrignon@gmail.com>
2021-04-27 21:09:35 +0200

    add a view linking tickets and event name and dateTime
    Signed-off-by: louis.arrignon <louis.arrignon@gmail.com>

--- Fast_Events/FastEvents/Models/Ticket.cs
+++ Fast_Events/FastEvents/Models/Ticket.cs
@@ -8,6 +8,9 @@ namespace FastEvents.Models
     public class Ticket : Interfaces.IObjectWithId
     {
         public long id { get; set; }
+        public string name { get; set; }
+        public DateTime start_date { get; set; }
+        public DateTime end_date { get; set; }
         public long eventId { get; set; }
         public string ownerUuid { get; set; }
         public string qrcFilename { get; set; }
