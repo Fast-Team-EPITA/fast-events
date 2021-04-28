5ddabdc7b186d5a6c0fcdd2fed064229054e2077
NielsBeeker <niels.beeker@hotmail.fr>
2021-04-28 18:41:29 +0200

    add repo and automapper

---
+++ Fast_Events/FastEvents/DataAccess/AutoMapperProfiles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace FastEvents.DataAccess
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<dbo.Stat, EfModels.Stat>();
+            CreateMap<EfModels.Stat, dbo.Stat>();
+
+            CreateMap<dbo.Event, EfModels.Event>();
+            CreateMap<EfModels.Event, dbo.Event>();
+
+            CreateMap<dbo.Ticket, EfModels.Ticket>();
+            CreateMap<EfModels.Ticket, dbo.Ticket>();
+
+            CreateMap<dbo.StatByEventModel, EfModels.EventView>();
+            CreateMap<EfModels.EventView, dbo.StatByEventModel>();
+
+        }
+    }
+}
---
+++ Fast_Events/FastEvents/DataAccess/StatRepository.cs
@@ -0,0 +1,24 @@
+﻿using FastEvents.dbo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FastEvents.DataAccess.EfModels;
+using Microsoft.Extensions.Logging;
+using AutoMapper;
+
+namespace FastEvents.DataAccess
+{
+    public class StatRepository : Repository<EfModels.Stat, dbo.Stat>, Interfaces.IStatRepository
+    {
+        public StatRepository(FastEventContext context, ILogger<StatRepository> logger, IMapper mapper) : base(context, logger, mapper)
+        {
+        }
+
+        public StatByEventModel GetByEvent(long eventId)
+        {
+            var result = _context.Stats.FirstOrDefault(x => x.EventId == eventId);
+            return _mapper.Map<dbo.StatByEventModel>(result);
+        }
+    }
+}
