ee21a86420bbe6e4bb4dfd46c6069c7c5411056f
louis.arrignon <louis.arrignon@gmail.com>
2021-04-28 18:49:05 +0200

    add ticketView mapper in MapperPreofiles
    Signed-off-by: louis.arrignon <louis.arrignon@gmail.com>

--- Fast_Events/FastEvents/DataAccess/AutoMapperProfiles.cs
+++ Fast_Events/FastEvents/DataAccess/AutoMapperProfiles.cs
@@ -22,6 +22,9 @@ namespace FastEvents.DataAccess
             CreateMap<dbo.StatByEventModel, EfModels.EventView>();
             CreateMap<EfModels.EventView, dbo.StatByEventModel>();
 
+            CreateMap<dbo.TicketView, EfModels.TicketView>();
+            CreateMap<EfModels.TicketView, dbo.TicketView>();
+
         }
     }
 }
