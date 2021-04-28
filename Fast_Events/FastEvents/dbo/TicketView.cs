e9d756fc4cbe889456ed30555526ccc250097a11
louis.arrignon <louis.arrignon@gmail.com>
2021-04-28 18:46:43 +0200

    add model in dbo corresponding to the ticketview efModel
    Signed-off-by: louis.arrignon <louis.arrignon@gmail.com>

---
+++ Fast_Events/FastEvents/dbo/TicketView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastEvents.dbo
+{
+    public class TicketView : Interfaces.IObjectWithId
+    {
+        public string Name { get; set; }
+        public long id { get; set; }
+        public long EventId { get; set; }
+        public string QrcFilename { get; set; }
+        public string OwnerUuid { get; set; }
+    }
+}
