8042d3ba1e8d2d2704437eb3e62f4492bffa4f50
louis.arrignon <louis.arrignon@gmail.com>
2021-04-30 18:33:49 +0200

    Change connection stirng to connect to Azure database

--- Fast_Events/FastEvents/DataAccess/EfModels/FastEventContext.cs
+++ Fast_Events/FastEvents/DataAccess/EfModels/FastEventContext.cs
@@ -26,7 +26,7 @@ namespace FastEvents.DataAccess.EfModels
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-SUJ7AFK;Initial Catalog=FastEvent;Trusted_Connection=True");
+                optionsBuilder.UseSqlServer("Server=tcp:fast-events.database.windows.net,1433;Database=FastEvent;User ID=FastTeam@fast-events;Password=CRi1CLaRue2021;Trusted_Connection=False;Encrypt=True;");
             }
         }
 
